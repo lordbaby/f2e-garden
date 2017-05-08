98ef8a72d5e73eafc603f01ec8c55905d5949893
Liangji <676708415@qq.com>
2017-05-08 16:43:47 +0800

    Update throttle.js

--- src/throttle.js
+++ src/throttle.js
@@ -1,5 +1,5 @@
 /**
- * 函数节流
+ * 函数节流 一开始先执行，后续每隔delay毫秒执行一次 适用于队列似的发送请求
  */
 export function throttle (fn, delay) {
   var self = fn;
@@ -21,5 +21,32 @@ export function throttle (fn, delay) {
       timer = null
       self.apply(me, args)
     }, delay || 500)
+  }
+}
+/**
+ * 函数节流 默认每隔delay秒执行一次，如果指定duration，则无视delay等待，直接执行
+ */
+export function throttle_V2 (fn, delay, duration) {
+  var self = fn;
+  var last_time;
+  var timer;
+
+  return function () {
+    var ctx = this;
+    var args = arguments;
+    var cur_time = new Date()
+
+    clearTimeout(timer)
+    if (!last_time) {
+      last_time = cur_time
+    }
+    if (duration && cur_time - last_time >= duration) {
+      self.apply(ctx, args)
+      last_time = cur_time
+    }else {
+      timer = setTimeout(function () {
+        self.apply(ctx, args)
+      }, delay | 500)
+    }
   }
 }
